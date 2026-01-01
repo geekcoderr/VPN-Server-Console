9b9b9c277bc7c040c4efebb9e7e373e38b641426
Nishant Maheshwari <mnishant222@gmail.com>
2026-01-02 03:58:37 +0530

    Switch to aiomysql to resolve ARM build issues

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ import os
 from .config import DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME
 
 # MySQL Async URL
-DATABASE_URL = f"mysql+asyncmy://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_async_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
